6eac3d2976cb8c0a1d75bb2eef3ade90b84679b7
Hanhelle <hanhelle@gmail.com>
2019-06-13 12:51:51 -0500

    Updates

---
+++ precache-manifest.855714abb1b1a66e16eec98c2370fa45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daac60660d9561199c969d0ece860a12",
+    "url": "/GDL002-framework-social-network/index.html"
+  },
+  {
+    "revision": "b10264535e28967893b6",
+    "url": "/GDL002-framework-social-network/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GDL002-framework-social-network/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b10264535e28967893b6",
+    "url": "/GDL002-framework-social-network/static/js/main.6d576846.chunk.js"
+  },
+  {
+    "revision": "6cead313ad965984df26",
+    "url": "/GDL002-framework-social-network/static/js/runtime~main.33323341.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GDL002-framework-social-network/static/media/logo.5d5d9eef.svg"
+  }
+]);
